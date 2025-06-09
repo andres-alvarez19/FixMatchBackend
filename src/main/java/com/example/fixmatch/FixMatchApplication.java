8b320560ff1c4f63367cdd495831cc5dda44fdd5
Andres Alvarez <a.alvarez08@ufromail.cl>
2025-06-09 05:16:31 -0400

    Iniciando Proyecto Spring Boot con Mysql

---
+++ src/main/java/com/example/fixmatch/FixMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.fixmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fixmatch/FixMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fixmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
